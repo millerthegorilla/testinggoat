d68cd968cbafc1f27c9df60ae62ede71cf3a1437
james miller <jamesstewartmiller@gmail.com>
2022-06-19 13:53:49 +0100

    found typo in views.py

--- lists/views.py
+++ lists/views.py
@@ -7,7 +7,7 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list})
+    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
     list_ = List.objects.create()

ea294d953316f73562a3673e96da74b588953d65
james miller <jamesstewartmiller@gmail.com>
2022-06-23 18:48:46 +0100

    finished javascript.  starting advanced section next

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,4 @@
-var initialize = function() {
-	$('input[name="text"]').on('keypress', function(){
-    		$('.has-error').hide()
-	});
-};
+$(document).ready(function () {
+  $('input[name="text"]').on('keypress', function(){ $('.has-error').hide(); });
+  $('input[name="text"]').on('click', function(){ $('.has-error').hide(); });
+});
